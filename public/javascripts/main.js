d04d5e93eaa4ae882b42fd7d1742c99f77133192
Matthew Eldredge <mattheweldredge@gmail.com>
2016-08-03 19:35:26 -0700

    include bootsrap

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,11 +6,16 @@ $.ajax({
         artist = (dataFromTheServer[i].artist);
         album = (dataFromTheServer[i].album);
         art = (dataFromTheServer[i].artwork_url);
-        $("#trackData").append('<tbody><tr><td class="tdata"><img class="album-artwork" src="" alt="artwork featured with this album"></td><th class="head">Artist: ' + '<em>' + artist + ',' + '</em>' + '</th><th class="head">Featured Album: ' + '<em>' + album + '</em>' + '</th></tr></tbody>');
+        sitename = (dataFromTheServer[i].sitename);
+        siteurl = (dataFromTheServer[i].siteurl);
+        $("#trackData").append('<tbody><tr><td class="tdata">' +
+            '<img class="album-artwork" src="" alt="artwork featured with this album"></td>' +
+            '<th class="head">Artist: ' + '<em>' + artist + ',' + '</em>' + '</th>' +
+            '<th class="head">Featured Album: ' + '<em>' + album + '</em>' + '</th>' +
+            //'<th class="head"><em>' + sitename + ': ' + '</em>' + siteurl '</th>' +
+            '</tr></tbody>');
         $('img.album-artwork').attr({src: art});
         $('#trackData, .head').addClass('col-xs-4 col-md-6');
-        $('').addClass('col-xs-4 col-md-2')
-
     }
 }).error(function(xhr, status, msg){
     alert("something went wrong");
