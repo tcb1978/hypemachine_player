6b3521a016de8932df7e04a09067e37ea71772f6
Matthew Eldredge <mattheweldredge@gmail.com>
2016-08-04 21:43:17 -0700

    INCLUDE if statement inside function to handle the undefined objects

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,17 +5,26 @@ $.ajax({
     for (var i = 0; i < dataFromTheServer.length; i++){
         artist = (dataFromTheServer[i].artist);
         album = (dataFromTheServer[i].album);
-        art = (dataFromTheServer[i].artwork_url);
+        art = (dataFromTheServer[i].artwork_url_sm);
         sitename = (dataFromTheServer[i].sitename);
-        siteurl = (dataFromTheServer[i].siteurl);
-        $("#trackData").append('<tbody><tr><td class="tdata">' +
-            '<img class="album-artwork" src="" alt="artwork featured with this album"></td>' +
-            '<th class="head">Artist: ' + '<em>' + artist + ',' + '</em>' + '</th>' +
-            '<th class="head">Featured Album: ' + '<em>' + album + '</em>' + '</th>' +
-            //'<th class="head"><em>' + sitename + ': ' + '</em>' + siteurl '</th>' +
-            '</tr></tbody>');
-        $('img.album-artwork').attr({src: art});
-        $('#trackData, .head').addClass('col-xs-4 col-md-6');
+        soundcloud = (dataFromTheServer[i].soundcloud_set);
+        type = (dataFromTheServer[i].type);
+        if (type == 'premiere'){
+            $("#trackData").append('<tbody><tr>' +
+                '<th class="head"><img class="album-artwork" src="" alt="artwork featured with this album"></th>' +
+                '<th class="head">Artist: </th>' +
+                '<th class="head">Featured Album: ' + '<em>' + album + '</em>' + '</th>' +
+                    //'<th class="head"><em>' + sitename + ': ' + '</em>' + siteurl '</th>' +
+                '</tr></tbody>' +
+                '<tbody><tr>' +
+                '<th><div class="head"><a class="siteurl" href="">' + 'SOUNDCLOUD' + '</a></div></th>' +
+                '<th><div class="head">' + artist + '</div></th>' +
+                '<th><div class="head">' + album + '</div></th>' +
+                '</tr></tbody>');
+            $('.siteurl').attr({href: soundcloud});
+            $('img.album-artwork').attr({src: art});
+            $('.head').addClass('col-xs-4 col-md-6');
+        }
     }
 }).error(function(xhr, status, msg){
     alert("something went wrong");
