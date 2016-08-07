83cf12076a6929a0814f865d54546e8cf9860c64
Matthew Eldredge <mattheweldredge@gmail.com>
2016-08-06 17:42:48 -0700

    table format

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -10,17 +10,22 @@ $.ajax({
         soundcloud = (dataFromTheServer[i].soundcloud_set);
         type = (dataFromTheServer[i].type);
         if (type == 'premiere'){
-            $("#trackData").append('<tbody><tr>' +
+            $("#trackData").append('<table width="400" cellpadding="10" cellspacing="5">' +
+                '<thead>' +
+                '<tr>' +
                 '<th class="head"><img class="album-artwork" src="" alt="artwork featured with this album"></th>' +
-                '<th class="head">Artist: </th>' +
-                '<th class="head">Featured Album: ' + '<em>' + album + '</em>' + '</th>' +
-                    //'<th class="head"><em>' + sitename + ': ' + '</em>' + siteurl '</th>' +
-                '</tr></tbody>' +
-                '<tbody><tr>' +
-                '<th><div class="head"><a class="siteurl" href="">' + 'SOUNDCLOUD' + '</a></div></th>' +
-                '<th><div class="head">' + artist + '</div></th>' +
-                '<th><div class="head">' + album + '</div></th>' +
-                '</tr></tbody>');
+                '<th class="head" scope="col">Artist: </th>' +
+                '<th class="head" scope="col">Featured Album: </th>' +
+                '</tr>' +
+                '</thead>' +
+                '<tbody>' +
+                '<tr>' +
+                    '<td class="head"><a class="siteurl" href="">' + 'SOUNDCLOUD' + '</a></div></td>' +
+                    '<td class="head">' + artist + '</div></td>' +
+                    '<td class="head">' + album + '</div></td>' +
+                '</tr>' +
+                '</body>' +
+            '</table>');
             $('.siteurl').attr({href: soundcloud});
             $('img.album-artwork').attr({src: art});
             $('.head').addClass('col-xs-4 col-md-6');
