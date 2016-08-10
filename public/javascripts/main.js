242f875b56bd69fc3f4837bf84d596bbca197f71
Matthew Eldredge <mattheweldredge@gmail.com>
2016-08-09 22:05:19 -0700

    add links

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -12,13 +12,14 @@ $.ajax({
                 var art = (data.artwork_url_sm);
                 var sitename = (data.sitename);
                 var soundcloud = (data.soundcloud_set);
+                var artistUrl = (data.premiere_url);
+                var label = (data.us_link);
                 var title = (data.tracks[j].title);
                 var audio = (data.tracks[j].stream_url);
                 $("#trackData").append(
                     '<tr>' +
-                    '<td><div class="col-xs-4 col-md-6"><img class="album-artwork" src="'+ art +'" alt="artwork featured with this album"></div></td>' +
-                    '<td><div class="col-xs-4 col-md-6"><a class="siteurl" href="'+ soundcloud +'">SOUNDCLOUD</a></div></td>' +
-                    '<td><div class="col-xs-4 col-md-6">' + artist + '</div></td>' +
+                    '<td><div class="col-xs-4 col-md-6"><a href="' + artistUrl + '"><img class="album-artwork" src="'+ art +'" alt="artwork featured with this album"></a></div></td>' +
+                    '<td><div class="col-xs-4 col-md-6"><a href="' + artistUrl + '">' + artist + '</a></div></td>' +
                     '<td><div class="col-xs-4 col-md-6">' + album + '</div></td>' +
                     '<td><div class="col-xs-4 col-md-6">' + title + '</div></td>' +
                     '<td><div class="col-xs-4 col-md-6">' +
@@ -26,6 +27,8 @@ $.ajax({
                     '<source src="' + audio + '" type="audio/ogg">' +
                     '<source src="' + audio + '" type="audio/mpeg">' +
                     'Your browser does not support the audio element.' +
+                    '<td><div class="col-xs-4 col-md-6"><a class="siteurl" href="'+ soundcloud +'">SOUNDCLOUD</a></div></td>' +
+                    '<td><div class="col-xs-4 col-md-6"><a href="' + label + '">' + label + '</a></div></td>' +
                     '</audio></div></td></tr>');
             }
         }
