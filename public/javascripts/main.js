8ee74b4504de01414edcba75afcc0c11337f8b72
Matthew Eldredge <mattheweldredge@gmail.com>
2016-08-09 20:04:12 -0700

    clean up

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,63 +3,30 @@ $.ajax({
     method: 'GET'
 }).success(function(dataFromTheServer){
     for (var i = 0; i < dataFromTheServer.length; i++){
-        artist = (dataFromTheServer[i].artist);
-        album = (dataFromTheServer[i].album);
-        art = (dataFromTheServer[i].artwork_url_sm);
-        sitename = (dataFromTheServer[i].sitename);
-        soundcloud = (dataFromTheServer[i].soundcloud_set);
-        type = (dataFromTheServer[i].type);
-        if (type == 'premiere'){
-            $("#trackData").append(
-                '<tr>' +
-                '<td><div class="col-xs-4 col-md-6"><img class="album-artwork" src="" alt="artwork featured with this album"></div></td>' +
-                '<td><div class="col-xs-4 col-md-6"><a class="siteurl" href="">SOUNDCLOUD</a></div></td>' +
-                '<td><div class="col-xs-4 col-md-6">' + artist + '</div></td>' +
-                '<td><div class="col-xs-4 col-md-6">' + album + '</div></td>' +
-                '</tr>');
-            $('.siteurl').attr({href: soundcloud});
-            $('img.album-artwork').attr({src: art});
-
-            for (var j = 0; j < dataFromTheServer[j].tracks.length; j++){
-                track = (dataFromTheServer[j].tracks.stream_url);
-                title = (dataFromTheServer[j].tracks.title);
-                    $("#trackData tr").append(
-                    '<td><div class="col-xs-4 col-md-6"><audio class="audio" src=""></audio></div></td>');
-                    $('audio.audio').attr({src: track});
-                    $('.track').append( title);
+        var data = dataFromTheServer[i];
+        var type = (data.type);
+        if (type == 'premiere' && data.tracks){
+            for (var j = 0; j < data.tracks.length; j++){
+                var artist = (data.artist);
+                var album = (data.album);
+                var art = (data.artwork_url_sm);
+                var sitename = (data.sitename);
+                var soundcloud = (data.soundcloud_set);
+                var title = (data.tracks[j].title);
+                $("#trackData").append(
+                    '<tr>' +
+                    '<td><div class="col-xs-4 col-md-6"><img class="album-artwork" src="'+ art +'" alt="artwork featured with this album"></div></td>' +
+                    '<td><div class="col-xs-4 col-md-6"><a class="siteurl" href="'+ soundcloud +'">SOUNDCLOUD</a></div></td>' +
+                    '<td><div class="col-xs-4 col-md-6">' + artist + '</div></td>' +
+                    '<td><div class="col-xs-4 col-md-6">' + album + '</div></td>' +
+                    '<td><div class="col-xs-4 col-md-6">' +
+                    //'<audio class="audio" src=""></audio>' +
+                    title + '</div></td>' +
+                    '</tr>');
             }
         }
     }
 }).error(function(xhr, status, msg){
     alert("something went wrong");
-});
-
-$.ajax({
-    url: 'https://api.hypem.com/v2/featured?key=swagger',
-    method: 'GET'
-}).success(function(dataFromTheServer){
-    for (var i = 0; i < dataFromTheServer.length; i++){
-        artist = (dataFromTheServer[i].artist);
-        album = (dataFromTheServer[i].album);
-        art = (dataFromTheServer[i].artwork_url_sm);
-        sitename = (dataFromTheServer[i].sitename);
-        soundcloud = (dataFromTheServer[i].soundcloud_set);
-        type = (dataFromTheServer[i].type);
-        if (type == 'premiere'){
-            $("#trackData").append(
-                '<tr>' +
-                '<td><div class="head artwork"><img class="album-artwork" src="" alt="artwork featured with this album"></div></td>' +
-                '<td><div class="head soundcloudlink"><a class="siteurl" href="">SOUNDCLOUD</a></div></td>' +
-                '<td><div class="head artist-album">' + artist + '</div></td>' +
-                '<td><div class="head artist-album">' + album + '</div></td>' +
-                '</tr>');
-            $('.siteurl').attr({href: soundcloud});
-            $('img.album-artwork').attr({src: art});
-            $('.head.artwork, .head.soundcloudlink').addClass('col-xs-8');
-            $('.head.artist-album').addClass('col-xs-4');
-        }
-    }
-}).error(function(xhr, status, msg){
-    alert("something went wrong");
-});
+})
 
