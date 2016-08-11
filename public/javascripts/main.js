51b5c842879326b702c3aae20860b5f2b526e041
Matthew Eldredge <mattheweldredge@gmail.com>
2016-08-11 12:00:14 -0700

    add unique id to tr

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,30 +6,40 @@ $.ajax({
         var data = dataFromTheServer[i];
         var type = (data.type);
         if (type == 'premiere' && data.tracks){
+            var artist, album, art, sitename = null;
             for (var j = 0; j < data.tracks.length; j++){
-                var artist = (data.artist);
-                var album = (data.album);
-                var art = (data.artwork_url_sm);
-                var sitename = (data.sitename);
-                var soundcloud = (data.soundcloud_set);
-                var artistUrl = (data.premiere_url);
+                artist = (data.artist);
+                album = (data.album);
+                art = (data.artwork_url_sm);
+                sitename = data.sitename;
+                var soundcloud = data.soundcloud_set;
+                var artistUrl = data.premiere_url;
                 var label = (data.us_link);
                 var title = (data.tracks[j].title);
                 var audio = (data.tracks[j].stream_url);
+                track_id = "track_row_" + data.tracks[j].sc_id;
                 $("#trackData").append(
-                    '<tr>' +
-                    '<td><div class="col-xs-4 col-md-6"><a href="' + artistUrl + '"><img class="album-artwork" src="'+ art +'" alt="artwork featured with this album"></a></div></td>' +
+                    '<tr id="'+track_id+'">' +
+                    '<td><div class="col-xs-4 col-md-6"><a href="' + artistUrl + '">' +
+                    '<img class="album-artwork" src="'+ art +'" alt="artwork featured with this album"></a></div></td>' +
                     '<td><div class="col-xs-4 col-md-6"><a href="' + artistUrl + '">' + artist + '</a></div></td>' +
                     '<td><div class="col-xs-4 col-md-6">' + album + '</div></td>' +
                     '<td><div class="col-xs-4 col-md-6">' + title + '</div></td>' +
-                    '<td><div class="col-xs-4 col-md-6">' +
+                    '<td><div class="col-xs-4 col-md-6"><a href="' + label + '">' + label + '</a></div></td>' +
+                    '<td><div class="col-xs-4 col-md-6"><a class="siteurl" href="'+ soundcloud +'">SOUNDCLOUD</a></div></td>' +
+                    '</tr>');
+                $("#playerElements" ).append('<li><div class="col-xs-4 col-md-6">' +
+                    '<a href="' + artistUrl + '">' +
+                    '<img class="album-artwork" src="'+ art +'" alt="artwork featured with this album"></a></div></li>' +
+                    '<li><div class="col-xs-4 col-md-6"><a href="' + artistUrl + '">' + artist + '</a></div></li>' +
+                    '<li><div class="col-xs-4 col-md-6">' + album + '</div></li>' +
+                    '<li><div class="col-xs-4 col-md-6">' + title + '</div></li>' +
+                    '<li><div class="col-xs-4 col-md-6">' +
                     '<audio controls="controls">' +
                     '<source src="' + audio + '" type="audio/ogg">' +
                     '<source src="' + audio + '" type="audio/mpeg">' +
                     'Your browser does not support the audio element.' +
-                    '<td><div class="col-xs-4 col-md-6"><a class="siteurl" href="'+ soundcloud +'">SOUNDCLOUD</a></div></td>' +
-                    '<td><div class="col-xs-4 col-md-6"><a href="' + label + '">' + label + '</a></div></td>' +
-                    '</audio></div></td></tr>');
+                    '</audio></div></li>');
             }
         }
     }
