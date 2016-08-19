f3b4587a1db18240717814d5fa60cc60195112a9
Matthew Eldredge <mattheweldredge@gmail.com>
2016-08-18 22:51:15 -0700

    add player

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -32,27 +32,29 @@ $.ajax({
         }
     }$('tr').on('click', function(e){
         e.preventDefault();
+        $('#playerElements tr:last-child' ).remove();
         var $this = $(this);
         var link_url = $this.find('a:first' ).attr('href')
         var link_art = $this.find('img:first' ).attr('src')
         var artistName = $this.find('.tdArtistUrlName').text()
+        var artistAlbum = $this.find('.tdArtistAlbum').text()
+        var artistTitle = $this.find('.tdArtistTitle').text()
         $('#playerElements' ).append(
             '<tr id="'+track_id+'">' +
-            '<td><div class="col-xs-4 col-md-6"><a href="' + artistUrl + '">' +
-            '<img class="album-artwork tdArt" src="'+ art +'" alt="artwork featured with this album"></a></div></td>' +
-            '<td><div class="col-xs-4 col-md-6"><a class="tdArtistUrlName" href="' + artistUrl + '">' + artist + '</a></div></td>' +
+                '<td><div class="col-xs-4 col-md-6"><a href="' + link_url + '">' +
+                '<img class="album-artwork tdArt" src="'+ link_art +'" alt="artwork featured with this album"></a></div></td>' +
+                '<td><div class="col-xs-4 col-md-6"><a class="tdArtistUrlName" href="' + link_url + '">Artist: "' + artistName + '"</a></div></td>' +
                 '<td>' +
                     '<div class="col-xs-4 col-md-6">' +
                         '<audio controls>' +
-                            '<source src="" type="audio/ogg">' +
-                            '<source src="" type="audio/mpeg">' +
+                            '<source src="" type="' + link_url + '">' +
+                            '<source src="" type="' + link_url + '">' +
                         '</audio>' +
             '       </div>>' +
                 '</td>>' +
-                '<td><div class="col-xs-4 col-md-6"><span  class="tdArtistAlbum">Album: ' + album + '</span></div></td>' +
-                '<td><div class="col-xs-4 col-md-6"><span  class="tdArtistTitle">Song: ' + title + '</span></div></td>' +
-            '</tr>'
-        );
+                '<td><div class="col-xs-4 col-md-6"><span  class="tdArtistAlbum">Album: "' + artistAlbum + '"</span></div></td>' +
+                '<td><div class="col-xs-4 col-md-6"><span  class="tdArtistTitle">Song: "' + artistTitle + '"</span></div></td>' +
+            '</tr>' );
         console.log('HERE HERE HERE HERE')
     });
 }).error(function(xhr, status, msg){
